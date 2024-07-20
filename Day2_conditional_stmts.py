038dd6f5a4e586bcfa6d36737ebec5655e2babe8
Md Sumaiya <128505774+sumaiya85@users.noreply.github.com>
2024-07-20 21:35:09 +0530

    Delete Day2_conditional_stmts.py

--- Day2_conditional_stmts.py
+++
@@ -1,7 +0,0 @@
-#conditional stmts:
-name=input("name")
-age=int(input("age"))
-if(age >= 18):
-    print(f"hello{name} you are eligible to vote")
-else:
-    print(f"hello{name} you are not eligible to vote")
