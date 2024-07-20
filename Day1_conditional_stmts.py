740bda7ac4c6a537bee031d6f310b7085206d295
Md Sumaiya <128505774+sumaiya85@users.noreply.github.com>
2024-07-20 21:53:39 +0530

    Add files via upload
    Day 1

---
+++ Day1_conditional_stmts.py
@@ -0,0 +1,7 @@
+#conditional stmts:
+name=input("name")
+age=int(input("age"))
+if(age >= 18):
+    print(f"hello{name} you are eligible to vote")
+else:
+    print(f"hello{name} you are not eligible to vote")
