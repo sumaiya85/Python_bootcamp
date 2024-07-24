ded78444f8d30d95a97b3d9160d1e33e13b860c7
Md Sumaiya <128505774+sumaiya85@users.noreply.github.com>
2024-07-24 20:22:03 +0530

    Day 5

---
+++ Day5_Ascii.py
@@ -0,0 +1,15 @@
+# to find ascii numbers of given characters
+print(ord('A')) #ord is order of a
+print(ord('A')+3)
+print(ord("0"))
+ 
+#from ascii to character
+print(chr(60))
+
+#
+print(ord('A'))
+for i in range(0,128):
+    print(chr(i),end=" ")
+for i in range(32,128):
+      print(chr(i),end=" ")
+
